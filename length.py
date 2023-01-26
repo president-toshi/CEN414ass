01cc85b9a94efec177c9fca597347c280cc3b55d
president-toshi <107267564+president-toshi@users.noreply.github.com>
2023-01-26 11:46:21 +0100

    Add files via upload

---
+++ length.py
@@ -0,0 +1,7 @@
+sentences = ["I am a boy.", "I am a girl.", "I am a man.", "I am a woman."]
+sentence_lengths = []
+
+for sentence in sentences:
+    sentence_lengths.append(len(sentence))
+
+print(sentence_lengths)
