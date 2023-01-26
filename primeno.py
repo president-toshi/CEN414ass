beb5999487ef668586f786ac780e38dbc4a39898
president-toshi <107267564+president-toshi@users.noreply.github.com>
2023-01-26 11:43:43 +0100

    Add files via upload

---
+++ primeno.py
@@ -0,0 +1,19 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+prime_numbers = []
+for i in range(1, 100001):
+    if is_prime(i):
+        prime_numbers.append(i)
+print(prime_numbers)
